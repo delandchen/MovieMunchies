64629ec822b53fce0a33019326558c3c6083d261
Deland Chen <80428701+delandchen@users.noreply.github.com>
2022-02-15 22:30:49 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import SearchBar from './component/Search';
-import React, { useEffect } from 'react'
-import MovieComponent from './component/Movie'
-import RecipeComponent from './component/Recipe'
-import { FetchMovies, FetchMovie, FetchPopular } from './api/movie'
-import RecipeApi from './api/recipe'
+import React, { useEffect } from 'react';
+import MovieComponent from './component/Movie';
+import RecipeComponent from './component/Recipe';
+import { FetchMovies, FetchMovie, FetchPopular } from './api/movie';
+import RecipeApi from './api/recipe';
 import Navigation from './component/Navigation';
 import Modal from './component/Modal';
 import Backdrop from './component/Backdrop';
@@ -14,26 +14,26 @@ import { Route, Routes } from 'react-router-dom';
 
 function App() {
 
-  const [appState, setAppState] = useState('Movie')  // Updates when changing api/components
-  const [searchValue, setSearchValue] = useState('') // Search value 
-  const [movie, setMovieObject] = useState([])  // Movie Object returned from API call
-  const [recipe, setRecipeObject] = useState([]) // Recipe Object from API call
-  const [modal, setModalIsActive] = useState(false)  
-  const [modalObject, setModalObject] = useState([])  // Movie object from API call
+  const [appState, setAppState] = useState('Movie');  // Updates when changing api/components
+  const [searchValue, setSearchValue] = useState(''); // Search value 
+  const [movie, setMovieObject] = useState([]);  // Movie Object returned from API call
+  const [recipe, setRecipeObject] = useState([]); // Recipe Object from API call
+  const [modal, setModalIsActive] = useState(false); 
+  const [modalObject, setModalObject] = useState([]);  // Movie object from API call
 
 
   // Fetches popular movies
   useEffect(async () => {
     const res = await FetchPopular()
     setMovieObject(res)
-  }, []) 
+  }, []);
 
 
   // Submits searchValue to API
   const handleSubmit = async (event) => {
     event.preventDefault();
     
-    if (searchValue === '') { return }
+    if (searchValue === '') { return; }
 
     else if (appState === 'Movie') { setMovieObject([]); 
       const res = await FetchMovies(searchValue); setMovieObject(res); }
@@ -50,16 +50,16 @@ function App() {
 
   // Set to render movies
   const setAppStateMovie = async () => {
-    setAppState('Movie')
+    setAppState('Movie');
   }
 
   // Set to render recipes
   const setAppStateRecipe = async () => {
-    setAppState('Snack')
+    setAppState('Snack');
   }
 
   const closeModal = () => {
-    setModalIsActive(false)
+    setModalIsActive(false);
   }
 
   // Renders the selected movie details
